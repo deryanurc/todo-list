371f2c9ade2877beb663c32d2be0661124f869ab
Derya Nur <nurderya791@gmail.com>
2022-08-31 16:45:41 +0300

    I've added local storage support, some changes to button styles, and a scrollbar to the todo list.

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ let input = document.getElementById("todo-add-input");
 let button = document.getElementById("todo-add-button");
 let list = document.getElementById("list");
 let doneList = document.getElementById("done-list");
-const todos = [];
-const doneTodos = [];
+const todos = getItemFromStorage() || [];
+const doneTodos = getItemFromStorage() || [];
 
+printTodos();
 button.addEventListener('click', () => {
    todos.push(input.value);
+   setItemToStorage(todos);
    input.value = "";
    printTodos();
 });
 function printTodos() {
    list.innerHTML = "";
    for (let i = 0; i < todos.length; i++) {
-      list.innerHTML += `<li>${todos[i]} <button id='${i}'>DONE</button></li>`
+      list.innerHTML += `<li>${todos[i]} <button id='${i}' class='list-buttons'>DONE</button></li>`
    }
    doneList.innerHTML = "";
    for (let a = 0; a < doneTodos.length; a++) {
-      doneList.innerHTML += `<li>${doneTodos[a]}</li>`;
+      doneList.innerHTML += `<li>${doneTodos[a]}<button id='${a}' class='list-buttons'>DELETE</button></li>`;
    };
 }
 
@@ -25,12 +27,26 @@ list.addEventListener('click', (e) => {
    if (e.target.tagName == 'BUTTON') {
       doneTodos.push(todos[e.target.id]);
       todos.splice(e.target.id, 1);
+      setItemToStorage();
       printTodos();
    }
-
-
-
 })
+doneList.addEventListener('click', (e) => {
+   if (e.target.tagName == 'BUTTON') {
+      console.log(e.target.id, e.target);
+      doneTodos.splice(e.target.id, 1);
+      setItemToStorage();
+      printTodos();
+   }
+})
+
+function getItemFromStorage() {
+   JSON.parse(localStorage.getItem("todos"));
+}
+function setItemToStorage() {
+   localStorage.setItem("todos", JSON.stringify(todos));
+   localStorage.setItem("doneTodos", JSON.stringify(doneTodos));
+}
 
 
 
